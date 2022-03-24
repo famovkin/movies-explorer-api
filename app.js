76cfd5b014d100b2e46fe9b418fc3e12a0a2ceef
Tolya Fam <famovkin@yandex.ru>
2022-03-25 02:45:09 +0300

    feat: добавить централизованную обработку ошибок

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ const { PORT = 3000 } = process.env;
 const routes = require('./routes');
 
 app.use(routes);
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
+
+  next();
+});
 
 async function startApp() {
   await mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
