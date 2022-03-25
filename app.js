64b3695b732be51f34a1a269daba66ad5dd62724
Tolya Fam <famovkin@yandex.ru>
2022-03-26 00:57:47 +0300

    feat: подключить логирование и cors

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const cors = require('cors');
 require('dotenv').config();
+
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const routes = require('./routes');
 
 const app = express();
 const { PORT = 3000 } = process.env;
-const routes = require('./routes');
 
+app.use(cors());
+app.use(requestLogger);
 app.use(routes);
+app.use(errorLogger);
 app.use(errors());
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
